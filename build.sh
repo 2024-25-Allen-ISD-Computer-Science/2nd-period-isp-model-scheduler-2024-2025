bb3c7ccd42d776985a1e50888644e8518fde8a41
Richard Jang <jang.richard7017@gmail.com>
2024-12-13 11:55:46 -0600

    updated status for build scripts

--- build.sh
+++ build.sh
@@ -6,26 +6,26 @@ IFS='.' read -r -a version_parts <<< "$current_version"
 ((version_parts[2]++))
 new_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
 version="$new_version"
-printf "%s\nUpdating to version: $version"
+printf "\nUpdating to version: $version"
 sed -i "4s/.*/$version/" README.md
 
 # Remove preexisting node modules
-printf "%s\nRemoving node packages"
+printf "\nRemoving node packages"
 rm -rf ./node_modules
 
 # Reinstall necessary node packages
-printf "%s\nInstalling node packages"
+printf "\nInstalling node packages"
 npm install
 
 # Build the production environment
-printf "%s\nBuilding production environment"
+printf "\nBuilding production environment"
 npm run build
 
 # CHANGE LATER TO DEPLOY THROUGH .github/workflows/deploy.yml
-printf "%s\nStashing current changes"
+printf "\nStashing current changes"
 git stash
 
-printf "%s\nPushing production build"
+printf "\nPushing production build"
 git add -f dist/
 git commit -m "VERSION $version"
 git subtree push --prefix dist origin production
