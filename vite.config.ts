d40ed83adea21d24b56dc1fa7397cd498d630a69
Richard Jang <jang.richard7017@gmail.com>
2025-03-22 18:24:31 -0500

    added correct basepath

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
