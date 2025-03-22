1199eacf6706cff93e164fc3a0fd0fc0bccf490e
Richard Jang <jang.richard7017@gmail.com>
2025-03-21 23:52:16 -0500

    removed files

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,18 +1,24 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useRef } from "react";
-// import ReactToPrint from "react-to-print";
+import { IoPhonePortraitOutline } from "react-icons/io5";
 
 export default function Layout() {
 
     const contentRef = useRef<HTMLDivElement>(null);
-
+    console.log("height: ", window.screen.height)
+    console.log("width: ", window.screen.width)
+    
     return <>
-        <div ref={contentRef} className="flex flex-col w-[100vw] max-w-[100vw] h-[100vh] max-h-[100vh] overflow-hidden bg-[#0d0d0d] relative">
+        <div ref={contentRef} className={`flex flex-col w-[100vw] max-w-[100vw] h-[100vh] max-h-[100vh] overflow-hidden bg-[#0d0d0d] relative ${window.screen.width <= 1200 ? "hidden" : ""}`}>
             <Navbar />
             <div className="flex flex-col w-full h-full p-8 overflow-hidden z-0">
                 <Outlet />
             </div>
+        </div>
+        <div className={`h-full w-full flex text-2xl items-center justify-center bg-black ${window.screen.width >= 1200 ? "hidden" : ""}`}>
+            <div><IoPhonePortraitOutline /></div>
+            <div>Please access this app on a computer.</div>
         </div>
     </>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,9 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
     }
   },
 })
